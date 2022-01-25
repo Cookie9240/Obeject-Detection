d8aba6b61aebf40f7616dacf9a47fed9cb1c4b35
Cookie9240 <90888292+Cookie9240@users.noreply.github.com>
2022-01-25 14:49:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+img = "";
+status = "";
+function preload() {
+    img = loadImage('dog_cat.jpg')
+}
+function setup() {
+    canvas=createCanvas(640 ,420);
+    canvas.center();
+    objectDetector= ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting Objects";
+}
+function draw() {
+    image(img, 0, 0, 640, 420); 
+    fill("#FF0000");
+    text("Dog", 45, 75);
+    noFill();
+    stroke("Blue")
+    rect(30, 60, 450, 350)
+
+    fill("blue");
+    text("Cat", 320, 120)
+    noFill();
+    stroke("red");
+    rect(300, 90, 270,320)
+}
+ function modelLoaded() {
+     console.log ("ModelLoaded !")
+     Status = true;
+     objectDetector.detect(img, gotResult);
+ }
+ function gotResult(error, results) {
+     if (error) {
+         console.log(error);
+     }
+     console.log(results);
+ }
